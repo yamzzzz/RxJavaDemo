df5e1a80e104594e7a442f4d29fc422531361cb2
yamzzz <yamzzz@8a697806-eaf5-4a18-b9a0-dcfafc55de8d>
2019-01-03 09:38:14 +0000

    delete

--- app/src/main/java/com/yamikrish/app/workmanagerdemo/utils/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.yamikrish.app.workmanagerdemo.utils
-
-class Constants {
-    companion object {
-        val EXTRA_TITLE = "TITLE_TEXT"
-        val EXTRA_MESSAGE = "MESSAGE_TEXT"
-        val EXTRA_OUTPUT_MESSAGE = "OUTPUT_TEXT"
-    }
-}
